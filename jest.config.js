1f39ab690a3392c3b712ebbde867e412f7f50e44
Andre <dreroips@gmail.com>
2021-12-10 23:27:53 -0300

    configuração do babel

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // simula o dom do browser
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
