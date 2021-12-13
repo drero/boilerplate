1ff65ee358d873cce70b81ebd0e4f14617684578
Andre <dreroips@gmail.com>
2021-12-13 11:50:42 -0300

    Configuração e estilização do Styled Components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,12 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Imagem do logo - React Avançado" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>TypeScript, ReactJS, NextJS e Styled Components</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="Imagem de jovem programando" />
+  </S.Wrapper>
 )
 
 export default Main
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -8,10 +8,17 @@ describe('<Main />', () => {
   it('should render the heading', () => {
 
     // Descreve o que queremos renderizar
-    render(<Main />)
+    const { container } =  render(<Main />)
 
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
