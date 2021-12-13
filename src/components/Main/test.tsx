819bb24de0326cc0e11674f2ce60cb3c7045c44b
Andre <dreroips@gmail.com>
2021-12-13 12:18:00 -0300

    ajustes doi styled-components no jest

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,21 +1,9 @@
-import { render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import Main from '.'
 
-// describe é usado para descrever o que será testado
 describe('<Main />', () => {
-  // it serve descrever  o que defato se espera do teste
-  it('should render the heading', () => {
 
-    // Descreve o que queremos renderizar
-    const { container } =  render(<Main />)
-
-    expect(
-      screen.getByRole('heading', { name: /react avançado/i })
-    ).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
-  })
   it('should render the colors correctly', () => {
     const { container } = render(<Main />)
 
